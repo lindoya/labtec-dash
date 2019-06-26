cf63a54ccf19375f27c30664a1c25904b2ee2862
jessiLeandro <jessi.profissional@gmail.com>
2019-06-26 08:31:40 -0300

    equip: service, validation, masks, index

--- src/services/equip.js
+++ src/services/equip.js
@@ -2,6 +2,29 @@ import axios from 'axios'
 import { BACKEND_URL } from './var'
 import { store } from '../App'
 
+export const newEquip = async (values) => {
+  const storeObject = store.getState()
+
+  const headers = {
+    token: storeObject.auth.token,
+    username: storeObject.auth.username,
+  }
+
+  let response = {}
+
+  await axios.post(`${BACKEND_URL}/api/equip`, values, { headers: headers }).then(
+    resp => {
+      response = resp
+    }
+  ).catch((error) => {
+    if (error.response) {
+      response = error.response
+    } else {
+      console.log('Error', error.message);
+    }
+  })
+  return response
+}
 
 export const getAllMarkByTypeService = async (type) => {
   const storeObject = store.getState()
